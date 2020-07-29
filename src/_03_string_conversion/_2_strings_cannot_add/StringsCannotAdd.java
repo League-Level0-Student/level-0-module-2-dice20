1860beb7985d31fc536edb938167ae6f00ca497a
root <root@7bad9f235c8c>
2020-07-29 01:25:00 +0000

    strings

--- src/_03_string_conversion/_2_strings_cannot_add/StringsCannotAdd.java
+++ src/_03_string_conversion/_2_strings_cannot_add/StringsCannotAdd.java
@@ -18,18 +18,21 @@ public class StringsCannotAdd {
 			// Did you notice 46 instead of 4 + 6 = 10?
 			
 			// 5) Ask the user to enter number 5 and hold it in the variable fiveAsString 
+			String fiveAsString = JOptionPane.showInputDialog("enter the number 5");
 			
 			// 6) Print the addition of the variables fourAsString, sixAsString, and fiveAsString 
-			
+			JOptionPane.showMessageDialog(null, fiveAsString +fourAsString +sixAsString);
 			//Did you notice 456 instead of 4 + 6 + 5 = 15?
 			
 			// Insert fourAsString inside the parenthesis of Integer.parseInt() and set the method equal to fourAsInt
 			// Hint: int fourAsInt = Integer.parseInt(fourAsString);
-			
+			 int fourAsInt = Integer.parseInt(fourAsString);
 			// 7) Do the same for sixAsString. 
-			
+			 int sixAsInt = Integer.parseInt(sixAsString);
+			 
+			 int fiveAsInt = Integer.parseInt(fiveAsString);
 			// 8) Print the addition of fourAsInt and sixAsInt using JOptionPane 
-			
+			 JOptionPane.showMessageDialog(null, fourAsInt +sixAsInt +fiveAsInt);
 			// Did you notice the value 10? If so, hurray! You just changed the type of your variable from String to int.
 			
 			// 9) Convert fiveAsString to fiveAsInt using Integer.parseInt();  
